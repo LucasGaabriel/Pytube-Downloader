5a6bfe296173c144c0e2c333130ab62f6c71157f
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2024-11-04 11:54:34 -0300

    Update PyTube Downloader

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Pytube-Downloader
+# Pytube Downloader
+
+Este projeto é uma aplicação de interface gráfica em Python para baixar vídeos e áudios do YouTube. Ele utiliza `customtkinter` para a interface e a biblioteca `pytubefix` para realizar os downloads.
+
+## Funcionalidades
+- Baixar o vídeo com a melhor resolução disponível.
+- Baixar apenas o áudio.
+
+## Aviso Legal
+Este projeto é oferecido apenas para fins educacionais e de uso pessoal. O autor não se responsabiliza por qualquer uso do programa que viole os Termos de Serviço e diretrizes do YouTube. Verifique sempre as regras e políticas do YouTube antes de realizar downloads de conteúdo.
+
+## Pré-requisitos
+- Python 3.12 ou superior.
+- Poetry para gerenciamento de dependências.
+
+## Instalação e Execução
+
+1. Ative o ambiente virtual:
+```
+poetry shell
+```
+
+2. Instale as dependências:
+```
+poetry install
+```
+
+3. Execute o programa:
+```
+poetry run python main.py
+```
--- main.py
+++ main.py
@@ -1,10 +1,61 @@
 from pytubefix import YouTube
+import customtkinter
 
-url = 'http://youtube.com/watch?v=2lAe1cqCOXo'
+class MyCheckboxFrame(customtkinter.CTkFrame):
+    def __init__(self, master, title, values):
+        super().__init__(master)
+        
+        self.title = title
+        self.values = values
+        self.checkboxes = []
+        
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
+        
+        for i, value in enumerate(self.values):
+            checkbox = customtkinter.CTkCheckBox(self, text=value)
+            checkbox.grid(row=i+1, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.checkboxes.append(checkbox)
+        
+    def get(self):
+        checked_checkboxes = []
+        for checkbox in self.checkboxes:
+            if checkbox.get() == 1:
+                checked_checkboxes.append(checkbox.cget("text"))
+        return checked_checkboxes
 
-yt = YouTube(url)
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("PyTube Downloader")
+        self.geometry("300x250")
+        self.grid_columnconfigure(0, weight=1)
+        
+        self.entry = customtkinter.CTkEntry(self, placeholder_text="Enter YouTube URL")
+        self.entry.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 
-print(yt.title)
+        self.checkbox_frame = MyCheckboxFrame(self, "Options", ["Audio", "Video"])
+        self.checkbox_frame.grid(row=1, column=0, padx=10, pady=10, ipady=10, sticky="nsw")
 
-# ys = yt.streams.get_highest_resolution()
-# ys.download()
+        self.button = customtkinter.CTkButton(self, text="Download", command=self.download_button_callback)
+        self.button.grid(row=2, column=0, padx=10, pady=10, sticky="ew")
+        
+    def download_button_callback(self):
+        url = self.entry.get()
+        audio = "Audio" in self.checkbox_frame.get()
+        video = "Video" in self.checkbox_frame.get()
+        self.download_youtube(url, audio, video)
+        
+    def download_youtube(self, url, audio=False, video=False):
+        yt = YouTube(url)
+
+        if audio:
+            audio = yt.streams.filter(only_audio=True).get_audio_only()
+            audio.download()
+        
+        if video:
+            video = yt.streams.filter(file_extension='mp4').get_highest_resolution()
+            video.download()
+
+app = App()
+app.mainloop()
